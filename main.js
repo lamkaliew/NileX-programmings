67c84b286f639bef52ea1392dbee77e52f0f0bb3
lamkaliew <lamkaliew@gmail.com>
2024-12-29 09:04:56 +0200

    version 8
    1.2

---
+++ main.js
@@ -0,0 +1,21 @@
+///* globe console document*/
+//
+//var AboutUs = document.getElementById("about-us");
+//var Disappearing = document.getElementById("disappear");
+//var GoBack = document.getElementById("go-back");
+//
+//
+//console.log(Disappearing);
+//AboutUs.onclick = function (){
+//    "use strick";
+//    Disappearing.style.display = "none";
+////    console.log("hello");
+//    
+//};
+//GoBack.onclick = function (){
+//    "use strick";
+//    console.log("hello");
+//    Disappearing.style.display = "none";
+//    
+//    
+//};
